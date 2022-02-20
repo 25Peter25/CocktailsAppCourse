6e054884969c8945d6e06bdbb8a0ab3e1ffd60b1
Peter <p.feling@gmail.com>
2022-02-20 14:31:12 +0100

    Spring Initializr

---
+++ src/main/java/com/peter/PeterCocktails/PeterCocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.peter.PeterCocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterCocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeterCocktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peter/PeterCocktails/PeterCocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.PeterCocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeterCocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
