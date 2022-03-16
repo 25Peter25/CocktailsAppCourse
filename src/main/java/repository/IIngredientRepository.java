d1b50325de06b3a649acdcc80e66681dd2b997ab
Peter <p.feling@gmail.com>
2022-03-16 21:39:31 +0100

    controller, models, html

--- src/main/java/repository/IIngredientRepository.java
+++ src/main/java/repository/IIngredientRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import model.TIngredient;
 
 public interface IIngredientRepository extends JpaRepository<TIngredient, Long>{
+	
+	
 
 }
